f303d4d5d0c934a222b46a2d461188c35cd25a6c
machado3105 <danilomachado3105@gmail.com>
2025-09-22 18:50:42 -0300

    melhorando o codigo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,9 +11,30 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
-};
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Acertou!");
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela("p", mensagemTentativas);
+    } else {
+        if(chute > numeroSecreto) {
+            exibirTextoNaTela("h1", "Errou!");
+            exibirTextoNaTela("p", `O número secreto é menor que ${chute}`);
+        } else {
+            exibirTextoNaTela("h1", "Errou!");
+            exibirTextoNaTela("p", `O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector("input");
+    chute.value = " ";
 }
